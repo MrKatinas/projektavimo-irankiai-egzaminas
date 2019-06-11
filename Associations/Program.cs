d68bb3f40a235cbd91d6c2199f1fc0c819be9c45
Mindaugas Butkus <mi.butkus@ktu.edu>
2019-06-11 21:35:19 +0300

    Initial commit

---
+++ Associations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Associations
+{
+	class Program{static void Main(string[] args){}}
+
+	public class Person
+	{
+		private string name;
+		private string lastName;
+
+		public void add() { }
+	}
+
+	public class Professor : Person
+	{
+		private string salary;
+
+		public void changeSalary() { }
+	}
+}
